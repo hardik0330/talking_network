cae6d9b29d2b872dc5245231c1d5ddb1d8d5a63d
Hardik KHURANA (EXT) <khurana-hardik@ldgt-091-hkhurana.designbuild.degremontgroup.net>
2019-06-06 16:38:27 +0530

    coordinate converter updated

--- coordinate_converter/coord_converter.py
+++ coordinate_converter/coord_converter.py
@@ -6,24 +6,38 @@ wgs84 = Proj(init = 'epsg:4326')
 IrishGrid = Proj(init = 'epsg:29902')
 
 #load in data
-with open(r'C:\Users\khurana-hardik\Documents\talking_network\Existing_Pipe.geojson', 'r') as f:
+with open(r'C:\Users\khurana-hardik\Documents\talking_network\trial.geojson', 'r') as f:
     data = json.load(f)
 
 #traverse data in json string
 target = []
 #coord = []
+f =open("trial.txt","w")
+f.write("{\n")
+f.write('"type": "FeatureCollection",\n')
+f.write('"name": "Existing_Pipe",\n')
+f.write('"features": [\n')
+
 for feature in data['features']:
      #print(feature['geometry']['type'])
      target = []
      target = feature['geometry']['coordinates']
+     objid = feature['properties']['OBJECTID']
+     diameter = feature['properties']['Diameter_m']
+     material = feature['properties']['Pipe_Mater']
+     length = feature['properties']['length_M']
+     type = feature['properties']['Type_']
+     f.write('{ "type": "Feature", "properties": { "OBJECTID": %d, "Diameter_m": %s, "Pipe_Mater": %s, "length_M": %s, "Type_": %s},' % (objid , diameter , material , length , type ))
      #print(target)
      coord = []
-
+     f.write('"geometry":{"type":"LineString" , "coordinates": [')
      for northing, easting in target:
          #print "Converting", easting, northing
          # Transform using pyproj (gives wrong answer)
          WGS84 = Proj(init='EPSG:4326')
          inp = Proj(init='EPSG:32645')
          x, y = transform(inp, WGS84, northing, easting)
+         f.write('[%f,%f]' % (x,y))
          coord.append([x,y])
+     f.write(']}')
      print(coord)
