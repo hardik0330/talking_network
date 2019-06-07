1d982e9748cb1c75214fbfbae4c93a858ee98076
vinayakkgarg <vinayakk.garg99@gmail.com>
2019-06-07 21:46:40 +0530

    plotting single layer all pipes

--- map.py
+++ map.py
@@ -4,7 +4,8 @@ import json
 
 m = folium.Map(location=[22.594264, 88.368343], zoom_start=13)
 
-overlay = os.path.join('Existing_Pipe2.json')
+overlay = os.path.join(
+    '/home/vinayakk/suez_internship/talking_network/coordinate_converter/Converted_file.geojson')
 
 # geojson overlay
 folium.GeoJson(overlay, name='Kolkata').add_to(m)
